c8baacec3db56f3eb55f535144ac171e93b4d1a8
Jonah Spear <jonahspear512@gmail.com>
2018-06-06 11:56:50 -0700

    we now write output to a csv

--- collect_data.py
+++ collect_data.py
@@ -1,9 +1,21 @@
-#!/bin/python
+#!/bin/python2
 # Scrape NBA player data from NBA website
 
+from __future__ import print_function
 import requests
 from bs4 import BeautifulSoup
+import os
 
+OUTPUT_FILE = 'postseason_stats_2018.csv'
+
+def output(line):
+	with open(OUTPUT_FILE, 'a') as f:
+		print(line, file=f)
+
+try:
+	os.remove(OUTPUT_FILE)
+except OSError:
+	pass
 
 page = requests.get("http://www.espn.com/nba/team/stats/_/name/cle")
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -11,5 +23,5 @@ soup = BeautifulSoup(page.content, 'html.parser')
 stat_rows = soup.find('table').find_all('tr')
 for row in stat_rows:
 	fields = [cell.get_text() for cell in row.find_all("td")]
-	print ','.join(fields) 
+	output(','.join(fields)) 
 
